81331fb53616a7890cfa5f983231b518d97bb7ba
Matty Muir <matty.muir@btinternet.com>
2023-12-24 15:41:20 +0000

    Resizing is handled better, janky fix for scrollbars.

--- client/index.js
+++ client/index.js
@@ -1,18 +1,43 @@
 "use strict"
 
-// Main
-var canvas = document.getElementById("mainCanvas")
-var w = canvas.getBoundingClientRect().width
-var h = canvas.getBoundingClientRect().height
+// === Globals ===
+var ctx = document.getElementById("mainCanvas").getContext("2d")
+var w = 0
+var h = 0
 
-// Drawing
-var ctx = canvas.getContext("2d")
+// === Event Handlers ===
+function UpdateDimensions()
+{
+    var canvasDiv = document.getElementById("canvasDiv")
+    w = canvasDiv.getBoundingClientRect().width
+    h = canvasDiv.getBoundingClientRect().height
 
-ctx.canvas.width = w
-ctx.canvas.height = h
+    ctx.canvas.width = w
+    ctx.canvas.height = h
+}
 
-ctx.moveTo(0, 0)
-ctx.lineTo(w, h)
-ctx.moveTo(0, h)
-ctx.lineTo(w, 0)
-ctx.stroke()
+function Refresh()
+{
+    UpdateDimensions()
+    console.log(`w: ${w} h: ${h}`)
+    OnDraw()
+}
+
+function OnResize(event)
+{
+    Refresh()
+}
+window.onresize = OnResize;
+
+// === Other Functions ===
+function OnDraw()
+{
+    ctx.moveTo(0, 0)
+    ctx.lineTo(w, h)
+    ctx.moveTo(0, h)
+    ctx.lineTo(w, 0)
+    ctx.stroke()
+}
+
+// === Main ===
+Refresh()
